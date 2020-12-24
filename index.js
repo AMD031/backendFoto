d980dc015b2c8b0ea34c0bae1ee9e50d477611bb
A.M.D <teclado1333@gmail.com>
2020-12-24 17:04:23 +0100

    make it better

--- index.js
+++ index.js
@@ -6,39 +6,83 @@ require('dotenv').config();
 app.use(cors());
 
 // Directorio Público
-app.use( express.static('public'));
-
+app.use(express.static('public'));
 const cloudinary = require('cloudinary').v2;
+// const { json } = require('express');
+
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+app.get('/api/get', async (req, res) => { 
+    const data = req.body.data;;
+    try {
+        // console.log(data);
+        cloudinary.api.resources_by_ids([...data],
+        (error, result) => {
+             res.json({ result });
+        });
+
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+    
+
+});
+
 app.post('/api/upload', async (req, res) => {
     try {
         const fileStr = req.body.data;
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {});
-        console.log(uploadResponse);
-        res.json({ res: uploadResponse});
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            overwrite: true,
+            invalidate: true,
+            // width: 810, height: 456, crop: "fill"
+        });
+        res.json({ res: uploadResponse });
     } catch (err) {
         res.status(500).json({ err });
     }
 });
 
-app.delete('/api/delete',  async (req, res) => {
+app.delete('/api/delete', async (req, res) => {
     try {
         const data = req.body.data;
-        console.log('dato:',data);  
-        await cloudinary.uploader.destroy(data, function(error,result) {
-        // console.log(result, error)
-     });
-        res.json({ msg: 'ok' });
+        await cloudinary.uploader.destroy(data, function (error, result) {
+            if (result) {
+                res.json({ msg: 'ok' });
+            }
+        });
     } catch (err) {
         res.status(500).json({ err });
     }
 
- })
+})
+
+
+app.put('/api/update', async (req, res) => {
+    try {
+        const imgkey = req.body.data.imgkey;
+        const img = req.body.data.img;
+        console.log(imgkey, '', img);
+        await cloudinary.uploader.destroy(imgkey,  async (error, result) => {
+            if (result) {
+                try {
+                    const uploadResponse = await cloudinary.uploader.upload(img, {});
+                    res.json({ res: uploadResponse });
+                } catch (err) {
+                    res.status(500).json({ err });
+                }
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+});
 
 
 app.get("/", (request, response) => {

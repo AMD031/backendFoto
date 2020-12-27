d04765a1a599377f09947671df87f01cd5e2a62f
A.M.D <teclado1333@gmail.com>
2020-12-27 19:48:35 +0100

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use( express.json() );
+// app.use( express.json() );
 app.use(cors());
 
 // app.use((req, res, next) => {
